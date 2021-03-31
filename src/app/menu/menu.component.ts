72dbfdb7f108883b73c7f847fcf151d2ea22dc4b
GUSTAVO-GUILHEN <gguilhen98@gmail.com>
2021-03-31 19:12:19 -0300

    refactor(NavBar):update NavBar

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -11,5 +11,17 @@ export class MenuComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  header_variable=false;
+  @HostListener("document:scroll")
+  scrollfunction(){
+      if(document.body.scrollTop > 0 || document.documentElement.scrollTop > 0 ){
+          this.header_variable=true;
+      }
+      else{
+          this.header_variable=false;
+      }
+  }
+
 
 }
