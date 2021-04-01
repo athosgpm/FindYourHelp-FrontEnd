1f788b63aab2a69c52f39f14a68f1419e2101edb
Velasco18 <velasco.osvaldo@yahoo.com.br>
2021-04-01 17:05:53 -0300

    refactor(change):create funtion to cadastrar

--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,62 +1,103 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
-import { NgForm, Validators, FormBuilder,FormsModule } from '@angular/forms';
+import { NgForm, Validators, FormBuilder, FormsModule } from '@angular/forms';
 import { ViewChild } from '@angular/core';
 import Swal from 'sweetalert2';
-
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastrar',
   templateUrl: './cadastrar.component.html',
-  styleUrls: ['./cadastrar.component.css']
+  styleUrls: ['./cadastrar.component.css'],
 })
-
-export class CadastrarComponent  {
- 
+export class CadastrarComponent implements OnInit {
+  user: User = new User();
+  confirmarSenha: string;
+  tipoUser: string;
   @ViewChild('stepper') stepper: any; // esse atributo é para uso de index nos steps
   hide = true; // Esse atributo é para revelar a senha
 
-  aceitomesmo:number =0; // Esse atributo é para mudar o valor do slider
-   /*injetar form control nos atributos e suas validações , essas validações baseadas no banco de dados*/
+  aceitomesmo: number = 0; // Esse atributo é para mudar o valor do slider
+  /*injetar form control nos atributos e suas validações , essas validações baseadas no banco de dados*/
   renda = new FormControl('', [Validators.required]);
   email = new FormControl('', [Validators.required, Validators.email]);
-  nome = new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(7)]);
-  tipoUsuario = new FormControl('', [Validators.required]);
-  senha = new FormControl('', [Validators.required, Validators.minLength(2), Validators.maxLength(7)]);
-  telefone = new FormControl ('', [ Validators.required,Validators.pattern("^[0-9]*$"),
-    Validators.minLength(11), Validators.maxLength(11)]);
+  nome = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(7),
+  ]);
+  tipoS = new FormControl('', [Validators.required]);
+  senha = new FormControl('', [
+    Validators.required,
+    Validators.minLength(2),
+    Validators.maxLength(7),
+  ]);
+  telefone = new FormControl('', [
+    Validators.required,
+    Validators.pattern('^[0-9]*$'),
+    Validators.minLength(11),
+    Validators.maxLength(11),
+  ]);
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit() {
+    window.scroll(0, 0);
+  }
 
- 
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value;
+  }
+  tipoDeUsuario(event: any) {
+    this.tipoUser = event.target.value;
+  }
+  cadastrar() {
+    this.user.tipoUsuario = this.tipoUser;
+    if (this.user.senhaUsuario != this.confirmarSenha) {
+      Swal.fire({
+        icon: 'error',
+        title: 'ops',
+        text: 'As senhas estão incorretas!',
+      });
+    } else {
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp;
+        this.router.navigate(['/login']);
+        Swal.fire({
+          icon: 'success',
+          title: 'show',
+          text: 'Usuário cadastrado com sucesso!',
+        });
+      });
+    }
+  }
 
   /*funcao para mover o step */
   move(index: number) {
     this.stepper.selectedIndex = index;
   }
 
-    /*funcao que pega o valor do atributo renda e retorna direção*/
-    testarRenda() {
-      if (this.renda.value == 2) {
-        Swal.fire({  
-          icon: 'error',  
-          title: 'Desculpe-nos',  
-          text: 'Seu formulario não preenche os requisitos para ser Empreendedor'  
-          
-        }) 
-        this.stepper.selectedIndex = 0;
-      }
+  /*funcao que pega o valor do atributo renda e retorna direção*/
+  testarRenda() {
+    if (this.renda.value == 2) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Desculpe-nos',
+        text: 'Seu formulario não preenche os requisitos para ser Empreendedor',
+      });
+      this.stepper.selectedIndex = 0;
     }
+  }
 
-     /*funcao de retorno de erros com o uso do parametro validators*/
+  /*funcao de retorno de erros com o uso do parametro validators*/
   getErrorMessageRenda() {
     if (this.renda.hasError('required')) {
       return 'A renda é obrigatoria';
     }
     return null;
-
   }
-  
+
   /*funcao de retorno de erros com o uso do parametro validators*/
   getErrorMessageEmail() {
     if (this.email.hasError('required')) {
@@ -72,16 +113,14 @@ export class CadastrarComponent  {
       return 'O nome é obrigatorio';
     }
     return null;
-
   }
 
   /*funcao de retorno de erros com o uso do parametro validators*/
   getErrorMessageTipo() {
-    if (this.tipoUsuario.hasError('required')) {
+    if (this.tipoS.hasError('required')) {
       return 'O Tipo de usuario é obrigatorio';
     }
     return null;
-
   }
 
   /*funcao de retorno de erros com o uso do parametro validators*/
@@ -90,26 +129,20 @@ export class CadastrarComponent  {
       return 'A senha é obrigatoria';
     }
     return null;
-
   }
-/*funcao de retorno de erros com o uso do parametro validators*/
-getErrorMessageTelefone() {
-  if (this.telefone.hasError('required')) {
-    return 'O Telefone é obrigatorio';
+  /*funcao de retorno de erros com o uso do parametro validators*/
+  getErrorMessageTelefone() {
+    if (this.telefone.hasError('required')) {
+      return 'O Telefone é obrigatorio';
+    }
+    return null;
   }
-  return null;
-
-}
-
-
 
   /*funcao de mudança de valor do atributo com o click do mouse*/
   Mudando(event: any) {
     if (event.checked == true) {
-      
       this.aceitomesmo = 1;
     } else {
-      
       this.aceitomesmo = 0;
     }
   }
@@ -121,25 +154,37 @@ getErrorMessageTelefone() {
 
   /*funcao que muda de cor usando referencia do styles.scss*/
   get controlColor1() {
-    return this.email.valid ? 'accent' : this.email.invalid ? 'warn' : 'primary';
+    return this.email.valid
+      ? 'accent'
+      : this.email.invalid
+      ? 'warn'
+      : 'primary';
   }
 
   /*funcao que muda de cor usando referencia do styles.scss*/
   get controlColor2() {
-    return this.telefone.valid ? 'accent' : this.telefone.invalid ? 'warn' : 'primary';
+    return this.telefone.valid
+      ? 'accent'
+      : this.telefone.invalid
+      ? 'warn'
+      : 'primary';
   }
 
   /*funcao que muda de cor usando referencia do styles.scss*/
   get controlColor3() {
-    return this.senha.valid ? 'accent' : this.senha.invalid ? 'warn' : 'primary';
+    return this.senha.valid
+      ? 'accent'
+      : this.senha.invalid
+      ? 'warn'
+      : 'primary';
   }
 
-  
   /*funcao que muda de cor usando referencia do styles.scss*/
   get controlColor4() {
-    return this.tipoUsuario.valid ? 'accent' : this.tipoUsuario.invalid ? 'warn' : 'primary';
+    return this.tipoS.valid
+      ? 'accent'
+      : this.tipoS.invalid
+      ? 'warn'
+      : 'primary';
   }
-
-
-
 }
