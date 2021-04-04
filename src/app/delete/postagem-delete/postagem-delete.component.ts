127ada5363b59c5d768412deddc6c5c20f574b2b
tatiantunes <tativive@hotmail.com>
2021-04-04 13:41:31 -0300

    feat(postagem): create crud  method delete

--- src/app/delete/postagem-delete/postagem-delete.component.ts
+++ src/app/delete/postagem-delete/postagem-delete.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Postagem } from 'src/app/model/Postagem';
+import { Tema } from 'src/app/model/Tema';
+import { PostagemService } from 'src/app/service/postagem.service';
+import { TemaService } from 'src/app/service/tema.service';
+import { environment } from './../../../environments/environment.prod';
 
 @Component({
   selector: 'app-postagem-delete',
@@ -7,9 +13,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostagemDeleteComponent implements OnInit {
 
-  constructor() { }
+  postagem: Postagem = new Postagem()
+  idPost:number
 
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private postagemService: PostagemService
+    
+  ) { }
+
+  ngOnInit(){
+    window.scroll(0,0)
+    if (environment.token === ''){
+
+      this.router.navigate(['/entrar']);
+    }
+    this.idPost = this.route.snapshot.params['id']
+    this.findByIdPostagem(this.idPost)
+    
   }
 
+  findByIdPostagem(id:number){
+    this.postagemService.getByIdPostagem(id).subscribe((resp:Postagem)=>{
+      this.postagem = resp
+    })
+  }
+
+  
+  apagar() {
+    this.postagemService.deletePostagem(this.idPost).subscribe(()=>{
+      alert('Essa postagem foi deletado com sucesso')
+      this.router.navigate(['/userpage'])
+    })
+  }
 }
